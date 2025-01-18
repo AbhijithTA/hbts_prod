0a3a43a80ab4d3bbb4f137b38d79911149d3aa05
lunu <nasrinlufna@gmail.com>
2025-01-18 18:31:01 +0530

    added home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,45 +2,230 @@ import { useState } from "react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isCityToursOpen, setIsCityToursOpen] = useState(false);
+  const [isDesertSafariOpen, setIsDesertSafariOpen] = useState(false);
+  const [isTicketopen, setisTicketopen] = useState(false);
+  const [isAdventureOpen, setIsAdventureOpen] = useState(false);
+
   return (
     <nav className="bg-white shadow-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-24">
-          <div className="flex  items-center ">
+          <div className="flex items-center">
             <a
               href="/"
-              className="flex  items-center text-[#077045] text-2xl font-bold"
+              className="flex items-center text-[#077045] text-2xl font-bold"
             >
-              <img src="/logo.png" className="h-20 sm:px-6"/>
-              <h1 className="Equila text-xl sm:text-2xl">Marit Tours</h1>
+              <img src="/logo.png" className="h-20 sm:px-6" alt="HBTS Logo" />
+              <h1 className="Equila text-xl sm:text-2xl">HBTS Travel and Tourism</h1>
             </a>
           </div>
-          <div className="hidden md:flex space-x-8">
-            <a
-              href="/package"
-              className="text-gray-700 hover:text-teal-500 font-medium"
+
+          {/* Desktop Menu */}
+          <div className="hidden md:flex space-x-8 uppercase">
+            <div
+              className="relative group"
+              onMouseEnter={() => setIsCityToursOpen(true)}
+              onMouseLeave={() => setIsCityToursOpen(false)}
             >
-              Packages
+              <a
+                href="/package"
+                className="flex items-center text-gray-700 hover:text-teal-500 font-medium"
+              >
+                City Tours
+                <svg
+                  className={`w-4 h-4 ml-1 transition-transform duration-300 ${
+                    isCityToursOpen ? "rotate-180" : ""
+                  }`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M19 9l-7 7-7-7"
+                  />
+                </svg>
+              </a>
+              {isCityToursOpen && (
+                <div className="absolute left-0 w-40 bg-white shadow-lg rounded-md z-10">
+                  <a
+                    href="/citytours/abu-dhabi"
+                    className="block px-4 py-2 text-gray-700 hover:text-teal-500"
+                  >
+                    Abu Dhabi
+                  </a>
+                  <a
+                    href="/citytours/dubai"
+                    className="block px-4 py-2 text-gray-700 hover:text-teal-500"
+                  >
+                    Dubai
+                  </a>
+                </div>
+              )}
+            </div>
+
+            <div
+              className="relative group"
+              onMouseEnter={() => setIsDesertSafariOpen(true)}
+              onMouseLeave={() => setIsDesertSafariOpen(false)}
+            >
+              <a
+                href="/desertsafari"
+                className="flex items-center text-gray-700 hover:text-teal-500 font-medium"
+              >
+                Desert Safari
+                <svg
+                  className={`w-4 h-4 ml-1 transition-transform duration-300 ${
+                    isDesertSafariOpen ? "rotate-180" : ""
+                  }`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M19 9l-7 7-7-7"
+                  />
+                </svg>
+              </a>
+              {isDesertSafariOpen && (
+                <div className="absolute left-0 w-56 bg-white shadow-lg rounded-md z-10">
+                  <a
+                    href="/desertsafari/morning"
+                    className="block px-4 py-2 text-gray-700 hover:text-teal-500"
+                  >
+                    Morning Desert Safari
+                  </a>
+                  <a
+                    href="/desertsafari/evening"
+                    className="block px-4 py-2 text-gray-700 hover:text-teal-500"
+                  >
+                    Evening Desert Safari
+                  </a>
+                  <a
+                    href="/desertsafari/dune-buggy"
+                    className="block px-4 py-2 text-gray-700 hover:text-teal-500"
+                  >
+                    Dune Buggy
+                  </a>
+                  <a
+                    href="/desertsafari/quad-biking"
+                    className="block px-4 py-2 text-gray-700 hover:text-teal-500"
+                  >
+                    Quad Biking
+                  </a>
+                </div>
+              )}
+            </div>
+
+            <div 
+            className="relative group"
+            onMouseEnter={() => setisTicketopen(true)}
+            onMouseLeave={() => setisTicketopen(false)}
+            >
+            <a
+              href="/tickets"
+              className="flex items-center text-gray-700 hover:text-teal-500 font-medium"
+            >
+              Ticket
+              <svg
+                  className={`w-4 h-4 ml-1 transition-transform duration-300 ${
+                    isTicketopen ? "rotate-180" : ""
+                  }`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M19 9l-7 7-7-7"
+                  />
+                </svg>
             </a>
+            {isTicketopen && (
+                <div className="absolute left-0 w-52 bg-white shadow-lg rounded-md z-10">
+                  <a
+                    href="/tickets/AttractionTickets"
+                    className="block px-4 py-2 text-gray-700 hover:text-teal-500"
+                  >
+                   Attraction Tickets
+                  </a>
+                  <a
+                    href="/tickets/ThemeParks"
+                    className="block px-4 py-2 text-gray-700 hover:text-teal-500"
+                  >
+                    Theme Parks
+                  </a>
+               </div>
+            )}
+
+            </div>
+
+            <div className="relative group"
+            onMouseEnter={() => setIsAdventureOpen(true)}
+            onMouseLeave={() => setIsAdventureOpen(false)}
+            >
             <a
-              href="/gallery"
+              href="/adventure"
+              className="flex items-center text-gray-700 hover:text-teal-500 font-medium"
+            >
+              Adventure
+
+              <svg
+                  className={`w-4 h-4 ml-1 transition-transform duration-300 ${
+                    isAdventureOpen ? "rotate-180" : ""
+                  }`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M19 9l-7 7-7-7"
+                  />
+                </svg>
+</a>
+                {isAdventureOpen && (
+                <div className="absolute left-0 w-52 bg-white shadow-lg rounded-md z-10">
+                  <a
+                    href="/adventure/helicopter"
+                    className="block px-4 py-2 text-gray-700 hover:text-teal-500"
+                  >
+                   Helicopter Tours
+                  </a>
+                  <a
+                    href="/adventure/hotairballoon"
+                    className="block px-4 py-2 text-gray-700 hover:text-teal-500"
+                  >
+                  Hot Air Balloon
+                  </a>
+                </div>
+              )}
+
+            </div>
+             
+            <a
+              href="/dhow-cruise"
               className="text-gray-700 hover:text-teal-500 font-medium"
             >
-              Gallery
-            </a>
-            <a
-              href="/workwithus"
-              className="text-gray-700 hover:text-teal-500 font-medium"
-            >
-              Join Us
-            </a>
-            <a
-              href="/contact"
-              className="text-gray-700 hover:text-teal-500 font-medium"
-            >
-              Contact Us
+              Dhow Cruise
             </a>
           </div>
+
+          {/* Mobile Menu Toggle */}
           <div className="flex items-center md:hidden">
             <button
               className="text-gray-500 hover:text-teal-500 focus:outline-none"
@@ -73,6 +258,8 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+
+      {/* Mobile Menu */}
       {isOpen && (
         <div className="md:hidden">
           <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import img1 from '/DeserSafari.jpg';
+import img2 from '/DubaiImage.jpg';
+import img3 from '/ThemePark.jpg';
+import img4 from '/Adventure.jpg';
+
+const images = [img1, img2, img3, img4];
+const texts = ['Desert Safari', 'City Tours', 'Theme Parks', 'Adventure'];
 
 const Hero = () => {
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000); // Change image and text every 3 seconds
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="relative h-[30vh] sm:h-[50vh] w-full overflow-hidden bg-black">
+      {/* Image Carousel */}
+      <AnimatePresence>
+        <motion.img
+          key={currentIndex}
+          src={images[currentIndex]}
+          alt={`Slide ${currentIndex}`}
+          className="absolute h-full w-full object-cover"
+          initial={{ scale: 1.5, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          exit={{ scale: 1.5, opacity: 0 }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+        />
+      {/* <Snowfall/> */}
+      </AnimatePresence>
+
+      {/* Centered Heading */}
+      <div className="absolute inset-0 flex items-center justify-center">
+        <AnimatePresence mode="wait">
+          <motion.h1
+            key={texts[currentIndex]}
+            className="text-white text-4xl md:text-[7rem] font-bold tracking-widest uppercase font-UnispaceBd"
+            style={{
+              textShadow: '3px 5px 7px rgba(0, 0, 0, 0.7)',
+            }}
+            initial={{ opacity: 0, letterSpacing: "0.3em" }}
+            animate={{ opacity: 1, letterSpacing: "0.7em" }}
+            exit={{ opacity: 0, letterSpacing: "0.5em" }}
+            transition={{
+              duration: 1,
+              ease: 'easeInOut',
+            }}
+          >
+            {texts[currentIndex]}
+          </motion.h1>
+        </AnimatePresence>
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
